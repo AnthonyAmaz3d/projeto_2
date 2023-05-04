be80f3f090b8a488a1ce51689fdb4e3ccac85cde
Anthony <anthonymaia00@gmail.com>
2023-05-04 16:29:32 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,43 @@
+const form = document.getElementById('form-agenda')
+const imgAprovado = '<img src="./images/aprovado.png" alt="emoji celebrando" />';
+const imgReprovado = '<img src="./images/reprovado.png" alt="emoji decepcionado" />';
+const contatos = [];
+const numero = [];
+const spamAprovado = '<span class= "resultado aprovado">Aprovado</span>';
+const spamReprovado = '<span class= "resultado reprovado">Reprovado</span>';
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumeroTelefone = document.getElementById('numero-telefone');
+
+    if (contatos.includes(inputNomeContato.value)) {
+        alert(`O número: ${inputNumeroTelefone.value} ja foi utilizado`);
+    } else {
+        contatos.push(inputNomeContato.value);
+        numero.push(parseFloat(inputNumeroTelefone.value));
+    
+        let linha = '<tr>';
+        linha += `<td>${inputNomeContato.value}</td>`;
+        linha += `<td>${inputNumeroTelefone.value}</td>`; 
+        linha += `</tr>`;
+    
+        linhas += linha;
+    }
+
+    inputNomeContato.value = '';
+    inputNumeroTelefone.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody'); 
+    corpoTabela.innerHTML = linhas; 
+}
